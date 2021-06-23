e82f6d562fe75289885fa888f2da931edb3e1c30
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2021-06-23 17:30:36 +0200

    Some improvements to connection script

--- conn-script/conn.py
+++ conn-script/conn.py
@@ -13,30 +13,31 @@ def AddConnectionFinish(client, result, _data):
     threading.Thread(target=client.add_connection_finish, args=(result,), daemon=True).run()
 
 def AddConnection():
-    interface = "eth4242"
-    dns = ["8.8.8.8"]
+    interface = "wwan0"
     print("Adding a new cell-modem connection...")
+
     conSettings = NM.SettingConnection.new()
-    conSettings.set_property("id", "myidyyy")
+    conSettings.set_property("id", "mycellular")
     conSettings.set_property("interface_name", interface)
     conSettings.set_property("uuid", str(uuid.uuid4()))
     conSettings.set_property("type", "gsm")
-    conSettings.set_property("metered", NM.Metered.YES)
     conSettings.set_property("interface_name", interface)
+
     gsmSettings = NM.SettingGsm.new()
-    #gsmSettings.set_property("auto_config", True)
-    #gsmSettings.set_property("apn", "internet")
+    gsmSettings.set_property("apn", "internet")
+
     ip4Settings = NM.SettingIP4Config.new()
     ip4Settings.set_property(NM.SETTING_IP_CONFIG_METHOD, "auto")
-    ip4Settings.add_address(NM.IPAddress.new(socket.AF_INET, "10.10.10.12", 24))
-    for d in dns: ip4Settings.add_dns(d)
+
     ip6Settings = NM.SettingIP6Config.new()
-    ip6Settings.set_property("method", "auto")
+    ip6Settings.set_property(NM.SETTING_IP_CONFIG_METHOD, "auto")
+
     connection = NM.SimpleConnection.new()
     connection.add_setting(conSettings)
     connection.add_setting(ip4Settings)
     connection.add_setting(ip6Settings)
     connection.add_setting(gsmSettings)
+
     print("Verifying connection parameters...")
     connection.verify()
     print("Saving connection...")
